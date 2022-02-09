19bc78ba1945db8087ebe48e48c89ae86e644cd5
Druvis Cukurs <druvis.cukurs@gmail.com>
2022-02-09 19:14:45 +0200

    gitignore resized images

--- scripts/sharp.js
+++ scripts/sharp.js
@@ -39,45 +39,51 @@ const resize = async (imageDir, resizedDir, sizes, quality) => {
       const publicUri = getPublicUri(uri);
 
       const transformer = sharp(uri);
-      const { width, height /*, format*/ } = await transformer.metadata();
-      // !!! FORCE WEBP !!!
-      const format = "webp";
+      const { width, height, format } = await transformer.metadata();
 
       // Do not re-size existing images.
       // if (ImageAttributes[publicUri]) {
       //   return;
       // }
 
-      if (format === "webp") {
-        transformer.webp({ quality });
-      } else if (format === "png") {
-        transformer.png({ quality });
+      // Have .png and .jpeg fallbacks for Safari & IE: https://caniuse.com/webp
+      if (format === "png") {
+        transformer.png({ quality, progressive: true });
       } else if (format === "jpeg") {
-        transformer.jpeg({ quality });
+        transformer.jpeg({ quality, progressive: true });
       }
 
       ImageAttributes[publicUri] = {
         width,
         height,
-
         // srcSet is currently not used (because of next/image).
         // srcSet: sizes.map(
         //   (width) => `/images/resized/${slug}-${width}${format} ${width}w`
         // ).join(", "),
-
         // Default to largest width.
         src: `${getPublicUri(
           resizedDir
         )}/${slug}-${DEFAULT_IMAGE_WIDTH}.${format}`,
       };
 
-      return Promise.all(
+      await Promise.all(
         sizes.map((size) => {
           transformer.resize(size);
 
           return transformer.toFile(`${resizedDir}/${slug}-${size}.${format}`);
         })
       );
+
+      // Create .webp images.
+      // transformer.webp({ quality });
+
+      // await Promise.all(
+      //   sizes.map((size) => {
+      //     transformer.resize(size);
+
+      //     return transformer.toFile(`${resizedDir}/${slug}-${size}.webp`);
+      //   })
+      // );
     })
   );
 
