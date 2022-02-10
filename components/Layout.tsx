7abacc96c55e4e6230e5c1332bd8a5f6d5ed174a
Druvis Cukurs <druvis.cukurs@gmail.com>
2022-02-10 16:19:47 +0200

    Rreference MAX_WIDTH  & include Tailwind class

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Head from "next/head";
 
-import { MAX_WIDTH } from "../styles/variables";
-
 import social from "../content/social.json";
 
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+
+import { MAX_WIDTH } from "../styles/variables";
 
 const NAME = "Terēze Medne";
 
@@ -21,7 +21,9 @@ export const Layout = ({ children }: { children: any }) => {
       </Head>
 
       <div
-        className={`px-4 w-full max-w-${MAX_WIDTH} mx-auto flex flex-col flex-1 center-items`}
+        className={`px-4 w-full ${
+          MAX_WIDTH === "7xl" && "max-w-7xl"
+        } mx-auto flex flex-col flex-1 center-items`}
       >
         <div className="py-4">
           <Header instagram={social.instagram} />
